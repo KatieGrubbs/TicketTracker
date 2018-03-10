3faae6ce9dc865f5fdfa3cc03cbaa55b20008667
Katie Grubbs <katie.grubbs@gmail.com>
2018-03-10 13:09:17 -0500

    Added TicketTrackerContext

---
+++ TicketTracker/Models/TicketTrackerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TicketTracker.Models
+{
+    public class TicketTrackerContext : DbContext
+    {
+        public TicketTrackerContext() : base("TicketTrackerConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<TicketTracker.Models.Ticket> Tickets { get; set; }
+        public System.Data.Entity.DbSet<TicketTracker.Models.Priority> Priorities { get; set; }
+        public System.Data.Entity.DbSet<TicketTracker.Models.Category> Categories { get; set; }
+    }
+}
