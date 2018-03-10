f85b2eb7017abe3a73e40d8c87f2ad4019c671c6
Katie Grubbs <katie.grubbs@gmail.com>
2018-03-10 00:29:19 -0500

    Created initial solution

---
+++ TicketTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicketTracker.Startup))]
+namespace TicketTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
