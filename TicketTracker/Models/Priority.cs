3626f1f9eca03c4703801ba7d5034c6d163c57cb
Katie Grubbs <katie.grubbs@gmail.com>
2018-03-10 13:06:24 -0500

    Added Ticket, Priority, and Category models

---
+++ TicketTracker/Models/Priority.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TicketTracker.Models
+{
+    public class Priority
+    {
+        [Key]
+        public int PriorityId { get; set; }
+
+        [Required]
+        public string PriorityCode { get; set; }
+
+        [Required]
+        public string PriorityName { get; set; }
+    }
+}
---
+++ TicketTracker/Models/Ticket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TicketTracker.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int TicketId { get; set; }
+
+        [Required]
+        public int PriorityId { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        public int CreatedBy { get; set; }
+
+        [Required]
+        public bool IsResolved { get; set; }
+    }
+}
