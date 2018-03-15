f280292344806a0c0521059dbdb94be39dc1f3e2
Katie Grubbs <katie.grubbs@gmail.com>
2018-03-15 11:26:58 -0400

    Added stored procedures to database project and moved Tables + StoredProcedures to dbo folder

---
+++ TicketTrackerDB/dbo/Stored Procedures/GetOpenTickets.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[GetOpenTickets]
+AS
+
+BEGIN 
+	SELECT
+		t.TicketId,
+		t.Subject,
+		t.Description,
+		t.SeverityLevelId,
+		s.SeverityLevelCode,
+		s.SeverityLevelName,
+		t.CategoryId,
+		c.CategoryName,
+		t.DateCreated,
+		t.ReporterId,
+		u.Email AS [ReporterName],
+		t.IsResolved,
+		t.IsDeleted
+	FROM Tickets AS t
+	JOIN SeverityLevels AS s ON t.SeverityLevelId = s.SeverityLevelId
+	JOIN Categories AS c ON t.CategoryId = c.CategoryId
+	JOIN AspNetUsers AS u ON t.ReporterId = u.Id
+	WHERE t.IsResolved = 0 AND t.IsDeleted = 0
+	ORDER BY t.DateCreated DESC
+END
