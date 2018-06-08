3e2490924dfec3fa14f71fb9efc6f49ff015c69e
Katie Grubbs <katie.grubbs@gmail.com>
2018-06-08 11:11:14 -0400

    Deleted old TicketTrackerDB files

--- TicketTrackerDB/Tables/Categories.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Categories]
-(
-	[CategoryId] INT NOT NULL IDENTITY (1,1), 
-    [CategoryName] NVARCHAR(50) NOT NULL
-
-	CONSTRAINT [PK_Categories] PRIMARY KEY ([CategoryId])
-)
--- TicketTrackerDB/Tables/SeverityLevels.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[SeverityLevels]
-(
-	[SeverityLevelId] INT NOT NULL IDENTITY (1,1), 
-    [SeverityLevelCode] VARCHAR(5) NOT NULL, 
-    [SeverityLevelName] VARCHAR(25) NOT NULL
-
-	CONSTRAINT [PK_SeverityLevels] PRIMARY KEY ([SeverityLevelId])
-)
