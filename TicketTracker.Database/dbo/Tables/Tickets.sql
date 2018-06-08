84b03a9a8c93c156c281060260e09e588db1c611
Katie Grubbs <katie.grubbs@gmail.com>
2018-06-08 11:02:04 -0400

    Renamed TicketTrackerDB to TicketTracker.Database

---
+++ TicketTracker.Database/dbo/Tables/Tickets.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Tickets]
+(
+	[TicketId] INT NOT NULL IDENTITY (1,1), 
+    [Subject] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(MAX) NOT NULL, 
+	[SeverityLevelId] INT NOT NULL DEFAULT 3, -- Moderate severity by default
+    [CategoryId] INT NOT NULL, 
+    [DateCreated] DATETIME NULL DEFAULT GETDATE(), 
+    [ReporterId] NVARCHAR(128) NULL , 
+    [IsResolved] BIT NOT NULL DEFAULT 0,
+	[IsDeleted] BIT NOT NULL DEFAULT 0, 
+
+	CONSTRAINT [PK_Tickets] PRIMARY KEY ([TicketId]),
+    CONSTRAINT [FK_Tickets_SeverityLevels] FOREIGN KEY ([SeverityLevelId])
+		REFERENCES dbo.SeverityLevels ([SeverityLevelId]),
+	CONSTRAINT [FK_Tickets_Categories] FOREIGN KEY ([CategoryId])
+		REFERENCES dbo.Categories ([CategoryId]),
+	CONSTRAINT [FK_Tickets_AspNetUsers] FOREIGN KEY ([ReporterId])
+		REFERENCES dbo.AspNetUsers ([Id])
+)
