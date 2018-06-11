2fde7b56bbb5e0c15ebb489d1f3267a8aa79eb7c
Katie Grubbs <katie.grubbs@gmail.com>
2018-06-11 15:03:26 -0400

    Refactored TicketsController to use Ticket service

--- TicketTracker/Controllers/TicketsController.cs
+++ TicketTracker/Controllers/TicketsController.cs
@@ -8,6 +8,8 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using TicketTracker.Models;
 using TicketTracker.Models.Utilities;
+using TicketTracker.Repositories;
+using TicketTracker.Services;
 using TicketTracker.ViewModels;
 
 namespace TicketTracker.Controllers
@@ -15,53 +17,51 @@ namespace TicketTracker.Controllers
     [Authorize]
     public class TicketsController : Controller
     {
+        private readonly ITicketService _ticketService;
+
+        public TicketsController()
+        {
+            _ticketService = new TicketService(new TicketRepository());
+        }
+
         // GET: Tickets/OpenTickets
+        [HttpGet]
         public ActionResult OpenTickets()
         {
             var openTickets = new List<TicketViewModel>();
 
-            using (var db = new ApplicationDbContext())
+            if (User.IsInRole(UserRoles.Administrator))
             {
-                if (User.IsInRole(UserRoles.Administrator))
-                {
-                    // call stored procedure and return a list of all open tickets
-                    openTickets = db.Database.SqlQuery<TicketViewModel>("GetOpenTickets").ToList();
-                }
-                else
-                {
-                    // call stored procedure and return a list of open tickets matching the user's id
-                    openTickets = db.Database.SqlQuery<TicketViewModel>("GetOpenTickets")
-                        .Where(t => t.ReporterId == User.Identity.GetUserId()).ToList();
-                }
-
-                return View(openTickets);
+                openTickets = _ticketService.GetOpenTickets();
             }
+            else
+            {
+                openTickets = _ticketService.GetOpenTicketsByUserId(User.Identity.GetUserId());
+            }
+
+            return View(openTickets);
         }
 
         // GET: Tickets/ResolvedTickets
+        [HttpGet]
         public ActionResult ResolvedTickets()
         {
             var resolvedTickets = new List<TicketViewModel>();
 
-            using (var db = new ApplicationDbContext())
+            if (User.IsInRole(UserRoles.Administrator))
             {
-                if (User.IsInRole("Admin"))
-                {
-                    // call stored procedure and return a list of all resolved tickets
-                    resolvedTickets = db.Database.SqlQuery<TicketViewModel>("GetResolvedTickets").ToList();
-                }
-                else
-                {
-                    // call stored procedure and return a list of resolved tickets matching the user's id
-                    resolvedTickets = db.Database.SqlQuery<TicketViewModel>("GetResolvedTickets")
-                        .Where(t => t.ReporterId == User.Identity.GetUserId()).ToList();
-                }
-
-                return View(resolvedTickets);
+                resolvedTickets = _ticketService.GetResolvedTickets();
             }
+            else
+            {
+                resolvedTickets = _ticketService.GetResolvedTicketsByUserId(User.Identity.GetUserId());
+            }
+
+            return View(resolvedTickets);
         }
 
         // GET: Tickets/Details/5
+        [HttpGet]
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -69,29 +69,25 @@ namespace TicketTracker.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            using (var db = new ApplicationDbContext())
+            var ticketViewModel = _ticketService.GetTicketViewModelById((int)id);
+
+            if (ticketViewModel == null)
             {
-                // call stored procedure to find ticket by id
-                var ticketViewModel = db.Database.SqlQuery<TicketViewModel>("GetTicketById @TicketId",
-                    new SqlParameter("TicketId", id)).SingleOrDefault();
-
-                if (ticketViewModel == null)
-                {
-                    return HttpNotFound();
-                }
-
-                return View(ticketViewModel);
+                return HttpNotFound();
             }
+
+            return View(ticketViewModel);
         }
 
         // GET: Tickets/Create
+        [HttpGet]
         public ActionResult Create()
         {
-            // make sure that dropdownlists in the create form are populated
+            // Make sure that dropdownlists in the create form are populated
             var ticketViewModel = new TicketViewModel
             {
-                SeverityLevels = GetSeverityLevels(),
-                Categories = GetCategories()
+                SeverityLevels = _ticketService.GetSeverityLevels(),
+                Categories = _ticketService.GetCategories()
             };
 
             return View(ticketViewModel);
@@ -102,34 +98,31 @@ namespace TicketTracker.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(TicketViewModel ticketViewModel)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                using (var db = new ApplicationDbContext())
-                {
-                    // create new ticket from viewmodel property values
-                    var ticket = new Ticket
-                    {
-                        TicketId = ticketViewModel.TicketId,
-                        Subject = ticketViewModel.Subject,
-                        Description = ticketViewModel.Description,
-                        SeverityLevelId = ticketViewModel.SeverityLevelId,
-                        CategoryId = ticketViewModel.CategoryId,
-                        ReporterId = User.Identity.GetUserId(),     // get logged in user
-                        IsResolved = ticketViewModel.IsResolved,
-                        IsDeleted = ticketViewModel.IsDeleted
-                    };
-
-                    db.Tickets.Add(ticket);
-                    db.SaveChanges();
-
-                    return RedirectToAction("OpenTickets", "Tickets");
-                }
+                return View(ticketViewModel);
             }
 
-            return View();
+            // Create new ticket from viewmodel property values
+            var ticket = new Ticket
+            {
+                Id = ticketViewModel.Id,
+                Subject = ticketViewModel.Subject,
+                Description = ticketViewModel.Description,
+                SeverityLevelId = ticketViewModel.SeverityLevelId,
+                CategoryId = ticketViewModel.CategoryId,
+                ReporterId = User.Identity.GetUserId(),     // Get logged in user
+                IsResolved = ticketViewModel.IsResolved,
+                IsDeleted = ticketViewModel.IsDeleted
+            };
+
+            _ticketService.CreateTicket(ticket);
+
+            return RedirectToAction("OpenTickets", "Tickets");
         }
 
         // GET: Tickets/Edit/5
+        [HttpGet]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -137,23 +130,18 @@ namespace TicketTracker.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            using (var db = new ApplicationDbContext())
+            var ticketViewModel = _ticketService.GetTicketViewModelById((int)id);
+
+            if (ticketViewModel == null)
             {
-                // call stored procedure to find ticket by id
-                var ticketViewModel = db.Database.SqlQuery<TicketViewModel>("GetTicketById @TicketId", 
-                    new SqlParameter("TicketId", id)).SingleOrDefault();
-
-                if (ticketViewModel == null)
-                {
-                    return HttpNotFound();
-                }
-
-                // make sure that dropdownlists in the edit form are populated
-                ticketViewModel.SeverityLevels = GetSeverityLevels();
-                ticketViewModel.Categories = GetCategories();
-
-                return View(ticketViewModel);
+                return HttpNotFound();
             }
+
+            // Make sure that dropdownlists in the edit form are populated
+            ticketViewModel.SeverityLevels = _ticketService.GetSeverityLevels();
+            ticketViewModel.Categories = _ticketService.GetCategories();
+
+            return View(ticketViewModel);
         }
 
         // POST: Tickets/Edit/5
@@ -161,35 +149,31 @@ namespace TicketTracker.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit(TicketViewModel ticketViewModel)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                using (var db = new ApplicationDbContext())
-                {
-                    // create new ticket from viewmodel property values
-                    var ticket = new Ticket
-                    {
-                        TicketId = ticketViewModel.TicketId,
-                        Subject = ticketViewModel.Subject,
-                        Description = ticketViewModel.Description,
-                        SeverityLevelId = ticketViewModel.SeverityLevelId,
-                        CategoryId = ticketViewModel.CategoryId,
-                        DateCreated = ticketViewModel.DateCreated,
-                        ReporterId = ticketViewModel.ReporterId,
-                        IsResolved = ticketViewModel.IsResolved,
-                        IsDeleted = ticketViewModel.IsDeleted
-                    };
-
-                    db.Entry(ticket).State = EntityState.Modified;
-                    db.SaveChanges();
-
-                    return RedirectToAction("OpenTickets", "Tickets");
-                }
+                return View(ticketViewModel);
             }
 
-            return View();
+            // Create new ticket from viewmodel property values
+            var ticket = new Ticket
+            {
+                Id = ticketViewModel.Id,
+                Subject = ticketViewModel.Subject,
+                Description = ticketViewModel.Description,
+                SeverityLevelId = ticketViewModel.SeverityLevelId,
+                CategoryId = ticketViewModel.CategoryId,
+                DateCreated = ticketViewModel.DateCreated,
+                ReporterId = ticketViewModel.ReporterId,
+                IsResolved = ticketViewModel.IsResolved,
+                IsDeleted = ticketViewModel.IsDeleted
+            };
+
+            _ticketService.EditTicket(ticket);
+
+            return RedirectToAction("OpenTickets", "Tickets");
         }
 
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = UserRoles.Administrator)]
         public ActionResult FlagAsResolved(int? id)
         {
             if (id == null)
@@ -199,21 +183,19 @@ namespace TicketTracker.Controllers
 
             using (var db = new ApplicationDbContext())
             {
-                // call stored procedure to find ticket by id
-                var ticketViewModel = db.Database.SqlQuery<TicketViewModel>("GetTicketById @TicketId",
-                    new SqlParameter("TicketId", id)).SingleOrDefault();
+                var ticketViewModel = _ticketService.GetTicketViewModelById((int)id);
 
                 if (ticketViewModel == null)
                 {
                     return HttpNotFound();
                 }
 
-                // mark ticket as resolved
+                // Mark ticket as resolved
                 ticketViewModel.IsResolved = true;
 
                 var ticket = new Ticket
                 {
-                    TicketId = ticketViewModel.TicketId,
+                    Id = ticketViewModel.Id,
                     Subject = ticketViewModel.Subject,
                     Description = ticketViewModel.Description,
                     SeverityLevelId = ticketViewModel.SeverityLevelId,
@@ -224,14 +206,13 @@ namespace TicketTracker.Controllers
                     IsDeleted = ticketViewModel.IsDeleted
                 };
 
-                db.Entry(ticket).State = EntityState.Modified;
-                db.SaveChanges();
+                _ticketService.EditTicket(ticket);
 
                 return RedirectToAction("OpenTickets", "Tickets");
             }
         }
 
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = UserRoles.Administrator)]
         public ActionResult FlagAsOpen(int? id)
         {
             if (id == null)
@@ -239,41 +220,35 @@ namespace TicketTracker.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            using (var db = new ApplicationDbContext())
+            var ticketViewModel = _ticketService.GetTicketViewModelById((int)id);
+
+            if (ticketViewModel == null)
             {
-                // call stored procedure to find ticket by id
-                var ticketViewModel = db.Database.SqlQuery<TicketViewModel>("GetTicketById @TicketId",
-                    new SqlParameter("TicketId", id)).SingleOrDefault();
-
-                if (ticketViewModel == null)
-                {
-                    return HttpNotFound();
-                }
-
-                // re-open ticket
-                ticketViewModel.IsResolved = false;
-
-                var ticket = new Ticket
-                {
-                    TicketId = ticketViewModel.TicketId,
-                    Subject = ticketViewModel.Subject,
-                    Description = ticketViewModel.Description,
-                    SeverityLevelId = ticketViewModel.SeverityLevelId,
-                    CategoryId = ticketViewModel.CategoryId,
-                    DateCreated = ticketViewModel.DateCreated,
-                    ReporterId = ticketViewModel.ReporterId,
-                    IsResolved = ticketViewModel.IsResolved,
-                    IsDeleted = ticketViewModel.IsDeleted
-                };
-
-                db.Entry(ticket).State = EntityState.Modified;
-                db.SaveChanges();
-
-                return RedirectToAction("ResolvedTickets", "Tickets");
+                return HttpNotFound();
             }
+
+            // Re-open ticket
+            ticketViewModel.IsResolved = false;
+
+            var ticket = new Ticket
+            {
+                Id = ticketViewModel.Id,
+                Subject = ticketViewModel.Subject,
+                Description = ticketViewModel.Description,
+                SeverityLevelId = ticketViewModel.SeverityLevelId,
+                CategoryId = ticketViewModel.CategoryId,
+                DateCreated = ticketViewModel.DateCreated,
+                ReporterId = ticketViewModel.ReporterId,
+                IsResolved = ticketViewModel.IsResolved,
+                IsDeleted = ticketViewModel.IsDeleted
+            };
+
+            _ticketService.EditTicket(ticket);
+
+            return RedirectToAction("ResolvedTickets", "Tickets");
         }
 
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = UserRoles.Administrator)]
         public ActionResult FlagAsDeleted(int? id)
         {
             if (id == null)
@@ -281,66 +256,32 @@ namespace TicketTracker.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            using (var db = new ApplicationDbContext())
+            var ticketViewModel = _ticketService.GetTicketViewModelById((int)id);
+
+            if (ticketViewModel == null)
             {
-                // call stored procedure to find ticket by id
-                var ticketViewModel = db.Database.SqlQuery<TicketViewModel>("GetTicketById @TicketId",
-                    new SqlParameter("TicketId", id)).SingleOrDefault();
-
-                if (ticketViewModel == null)
-                {
-                    return HttpNotFound();
-                }
-
-                // soft-delete ticket
-                ticketViewModel.IsDeleted = true;
-
-                var ticket = new Ticket
-                {
-                    TicketId = ticketViewModel.TicketId,
-                    Subject = ticketViewModel.Subject,
-                    Description = ticketViewModel.Description,
-                    SeverityLevelId = ticketViewModel.SeverityLevelId,
-                    CategoryId = ticketViewModel.CategoryId,
-                    DateCreated = ticketViewModel.DateCreated,
-                    ReporterId = ticketViewModel.ReporterId,
-                    IsResolved = ticketViewModel.IsResolved,
-                    IsDeleted = ticketViewModel.IsDeleted
-                };
-
-                db.Entry(ticket).State = EntityState.Modified;
-                db.SaveChanges();
-
-                return RedirectToAction("OpenTickets", "Tickets");
+                return HttpNotFound();
             }
-        }
-        
-        private IEnumerable<SelectListItem> GetSeverityLevels()
-        {
-            using (var db = new ApplicationDbContext())
+
+            // Soft-delete ticket
+            ticketViewModel.IsDeleted = true;
+
+            var ticket = new Ticket
             {
-                var severityLevels = db.SeverityLevels.Select(s => new SelectListItem
-                {
-                    Value = s.SeverityLevelId.ToString(),
-                    Text = s.SeverityLevelCode + " - " + s.SeverityLevelName
-                }).ToList();
+                Id = ticketViewModel.Id,
+                Subject = ticketViewModel.Subject,
+                Description = ticketViewModel.Description,
+                SeverityLevelId = ticketViewModel.SeverityLevelId,
+                CategoryId = ticketViewModel.CategoryId,
+                DateCreated = ticketViewModel.DateCreated,
+                ReporterId = ticketViewModel.ReporterId,
+                IsResolved = ticketViewModel.IsResolved,
+                IsDeleted = ticketViewModel.IsDeleted
+            };
 
-                return new SelectList(severityLevels, "Value", "Text");
-            }
-        }
+            _ticketService.EditTicket(ticket);
 
-        private IEnumerable<SelectListItem> GetCategories()
-        {
-            using (var db = new ApplicationDbContext())
-            {
-                var categories = db.Categories.OrderBy(c => c.CategoryName).Select(c => new SelectListItem
-                {
-                    Value = c.CategoryId.ToString(),
-                    Text = c.CategoryName
-                }).ToList();
-
-                return new SelectList(categories, "Value", "Text");
-            }
+            return RedirectToAction("OpenTickets", "Tickets");
         }
     }
 }
