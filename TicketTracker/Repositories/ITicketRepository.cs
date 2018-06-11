43d1a7b8a03748680f5629725e46177335485a72
Katie Grubbs <katie.grubbs@gmail.com>
2018-06-11 15:01:48 -0400

    Added Ticket repository

---
+++ TicketTracker/Repositories/ITicketRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using TicketTracker.Models;
+
+namespace TicketTracker.Repositories
+{
+    public interface ITicketRepository
+    {
+        IEnumerable<Ticket> GetAll();
+
+        Ticket GetById(int id);
+
+        void Add(Ticket ticket);
+
+        void Update(Ticket ticket);
+
+        void Delete(int id);
+    }
+}
---
+++ TicketTracker/Repositories/TicketRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using TicketTracker.Models;
+
+namespace TicketTracker.Repositories
+{
+    public class TicketRepository : ITicketRepository
+    {
+        public IEnumerable<Ticket> GetAll()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                return db.Tickets.ToList();
+            }
+        }
+
+        public Ticket GetById(int id)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                return db.Tickets.Find(id);
+            }
+        }
+
+        public void Add(Ticket ticket)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                db.Tickets.Add(ticket);
+                db.SaveChanges();
+            }
+        }
+
+        public void Update(Ticket ticket)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                db.Entry(ticket).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var ticket = db.Tickets.Find(id);
+                db.Tickets.Remove(ticket);
+                db.SaveChanges();
+            }
+        }
+    }
+}
